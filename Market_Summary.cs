ced20eef533d7de4ee0776b509a23ac3619d054f
Abdul Moeed Khan <bcsf11a081@pucit.edu.pk>
2015-01-20 23:08:27 +0500

    Web service code files..

---
+++ Market_Summary.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IntelliStock_WebService
+{
+    public class Market_Summary
+    {
+        public String marketStatus { get; set; }
+        public double total_Advanced { get; set; }
+        public double current_KSE100 { get; set; }
+        public double current_AllShare { get; set; }
+        public double current_KSE30 { get; set; }
+        public double current_KMI30 { get; set; }
+
+
+        public double market_Volume { get; set; }
+        public double total_Declined { get; set; }
+        public double high_KSE100 { get; set; }
+        public double high_AllShare { get; set; }
+        public double high_KSE30 { get; set; }
+        public double high_KMI30 { get; set; }
+
+        public double market_Value { get; set; }
+        public double total_Unchanged { get; set; }
+        public double low_KSE100 { get; set; }
+        public double low_AllShare { get; set; }
+        public double low_KSE30 { get; set; }
+        public double low_KMI30 { get; set; }
+
+        public double market_Trades { get; set; }
+        public double total { get; set; }
+        public double change_KSE100 { get; set; }
+        public double change_AllShare { get; set; }
+        public double change_KSE30 { get; set; }
+        public double change_KMI30 { get; set; }
+
+        public DateTime currentDateTime { get; set; }
+    }
+}
---
+++ ValuesController.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace IntelliStock_WebService.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        public class User
+        {
+            public string name { get; set; }
+            public int id { get; set; }
+
+        }
+        // GET api/values             
+        public Object Get()
+        {
+
+       //     User obj = new User { name = "Moeed", id = 1 };
+    
+            Web_Scraper obj = new Web_Scraper();
+            obj.startScrapping();
+
+            return obj.symbol_list;  
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
